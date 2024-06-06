bd0a3ad2642c4c1ea91706c62a50fbc35007db8e
Utsab Haldar <utsabhaldar88888@gmail.com>
2024-06-06 17:28:04 +0530

    Sign Up page created

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:blog_app/core/theme/theme.dart';
+import 'package:blog_app/features/auth/presentation/pages/signup_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,16 +9,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Placeholder(),
+      debugShowCheckedModeBanner: false,
+      title: 'BlogSphere',
+      theme: AppTheme.darkThemeMode,
+      home: const SignUpPage(),
     );
   }
 }
